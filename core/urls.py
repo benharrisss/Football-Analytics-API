639714acbac5451f71104b269151f73f2b0ead2a
benharrisss <sc23bh2@leeds.ac.uk>
2026-03-10 22:40:56 +0000

    additional-url-for-render

--- core/urls.py
+++ core/urls.py
@@ -28,6 +28,8 @@ router.register(r'teams', TeamViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
